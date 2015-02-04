5002e97f2f72064b77afaf4eedf13df98a4e32b6
Piotr Mlynarski <Piotrek@MacBook-Pro-Piotr.local>
2015-02-04 22:27:02 +0100

    Initial Commit

---
+++ Core Data/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Core Data
+//
+//  Created by Piotr Mlynarski on 04.02.2015.
+//  Copyright (c) 2015 Piotr Mlynarski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
