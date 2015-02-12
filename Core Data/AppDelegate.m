2fdb15bbd4756bd7088e9b0d1d2ab787ae44ec71
AppStarter <krzysztof.holtyn@gmail.com>
2015-02-12 19:27:45 +0100

    tera działa

--- Core Data/AppDelegate.m
+++ Core Data/AppDelegate.m
@@ -28,31 +28,29 @@
      [NSArray arrayWithObjects:@"Aleksandra", @"Blendowska", @"95010900991", nil]
      , nil];
     
-    Osoba *newPerson = [Osoba createObject];
     for (NSArray *osoby in peopleToAdd) {
-        
+        Osoba *newPerson = [Osoba createObject];
+
         newPerson.imie = [osoby objectAtIndex:0];
         newPerson.nazwisko = [osoby objectAtIndex:1];
         newPerson.pesel = [osoby objectAtIndex:2];
         NSLog(@"Tworzenie: %@ %@ %@...", newPerson.imie, newPerson.nazwisko, newPerson.pesel);
 
+        [Osoba saveDatabase];
     }
     
-    //[Osoba saveDatabase];
-    
- 
-    /*wczyt = [Osoba readAllObjects];
-    for (NSArray *osoby2 in wczyt) {
-        //NSLog(@"Znaleziono w bazie danych: %@....", [osoby2 objectAtIndex:0]);
-    }*/
+    wczyt = [Osoba readAllObjects];
+    for (Osoba *osoba in wczyt) {
+        NSLog(@"Znaleziono w bazie danych: %@....", osoba.nazwisko);
+    }
 
-    
+    [Osoba removeAllObjects];
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    //[self createSampleData];
+    [self createSampleData];
     //[self.window makeKeyAndVisible];
     
     return YES;
@@ -111,17 +109,18 @@
     
     // Create the coordinator and store
     
-    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+   
     NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"Core_Data.sqlite"];
+    
     NSError *error = nil;
-    NSString *failureReason = @"There was an error creating or loading the application's saved data.";
+     _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
     if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
         // Report any error we got.
-        NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-        dict[NSLocalizedDescriptionKey] = @"Failed to initialize the application's saved data";
-        dict[NSLocalizedFailureReasonErrorKey] = failureReason;
-        dict[NSUnderlyingErrorKey] = error;
-        error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:9999 userInfo:dict];
+//        NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+//        dict[NSLocalizedDescriptionKey] = @"Failed to initialize the application's saved data";
+//        dict[NSLocalizedFailureReasonErrorKey] = failureReason;
+//        dict[NSUnderlyingErrorKey] = error;
+//        error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:9999 userInfo:dict];
         // Replace this with code to handle the error appropriately.
         // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
         NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
